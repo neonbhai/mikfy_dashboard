79766d4ef03fda7ad1493d0b039c9ffae38f7cdb
Someshwar Tripathi <someshwartripathi8@gmail.com>
2025-10-09 03:13:00 +0530

    Add search image

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function SearchPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] gap-8">
+      {/* Search illustration */}
+      <Image
+        src="/brand/search.jpg"
+        alt="Search illustration"
+        width={200}
+        height={200}
+        className="object-contain"
+      />
+
       {/* Main message */}
       <div className="text-center">
         <h1 className="text-[32px] font-normal text-[#151515] leading-[1.2]">
@@ -20,4 +30,3 @@ export default function SearchPage() {
     </div>
   );
 }
-
