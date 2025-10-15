a296541924b161c1c711c748094ccde23bc9a51e
Someshwar Tripathi <someshwartripathi8@gmail.com>
2025-10-15 22:19:45 +0530

    Product image priority

--- src/components/inventory/ProductImage.tsx
+++ src/components/inventory/ProductImage.tsx
@@ -9,10 +9,16 @@ interface ProductImageProps {
 const ProductImage: React.FC<ProductImageProps> = ({ src, alt }) => {
   return (
     <div className="relative w-[95px] h-[95px] rounded-[10px] overflow-hidden bg-gray-100 flex-shrink-0">
-      <Image src={src} alt={alt} fill className="object-cover" sizes="95px" />
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        className="object-cover"
+        sizes="95px"
+        priority
+      />
     </div>
   );
 };
 
 export default ProductImage;
-
